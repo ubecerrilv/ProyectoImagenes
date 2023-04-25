0055484e07d62e756ac8bec0b8355a55c50bc29d
marco <marco@LaptopMarcos>
2023-04-25 11:43:22 -0500

    Primeras declaraciones de ventanas.

--- ProyectoTID/src/imagenes/gui/VentanaPrincipal.java
+++ ProyectoTID/src/imagenes/gui/VentanaPrincipal.java
@@ -1,9 +1,30 @@
 package imagenes.gui;
 
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 @SuppressWarnings("serial")
 public class VentanaPrincipal extends VentanaAGeneral{
+	
+	JPanel central, derecho, imagenCentral, arriba, arribaBotones;
+	JFileChooser imagen;
+	JButton regresar, siguiente;
+	
+	public VentanaPrincipal(){
+		super("Proyecto Tratamiento Imagenes");
+		central = new JPanel(new GridLayout(1,1));
+		central.setBorder(new EmptyBorder(5,5,5,5));
+		derecho = new JPanel(new GridLayout(1,1));
+		derecho.setBorder(new EmptyBorder(5,5,5,5));
+		arriba = new JPanel(new GridLayout(1,3));
+		arriba.setBorder(new EmptyBorder(5,5,5,5));
+		
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
